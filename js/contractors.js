1d6779aa5a5d9888f1f68d62d01a71ecf2cd0728
CyberFinn <stephendiavolo@gmail.com>
2024-08-10 18:59:28 +0200

    Fix: Contractors.txt

--- js/contractors.js
+++ js/contractors.js
@@ -9,19 +9,6 @@ window.onload = function() {
             fileDisplayArea.innerHTML = parseMarkdown(xhr.response)
         }
  };
- function parseMarkdown(markdownText) {
-    // Replace Markdown headings (e.g., # Heading) with HTML <h1> tags
-    markdownText = markdownText.replace(/^# (.+)/gm, '<h1>$1</h1>');
-  
-    // Replace emphasis (e.g., *italic*) with HTML <em> tags
-    markdownText = markdownText.replace(/\*([^*]+)\*/g, '<em>$1</em>');
-    
-    // Replace ordered list items (e.g., "1. Item") with <li> tags
-    markdownText = markdownText.replace(/^(\d+\.)\s+(.+)/gm, '<li>$2</li>');
-  
-    return markdownText;
-  }
-
   function parseMarkdown(markdownText) {
     // Replace Markdown headings (e.g., # Heading) with HTML <h1> tags
     markdownText = markdownText.replace(/^# (.+)/gm, '<h1>$1</h1>');
