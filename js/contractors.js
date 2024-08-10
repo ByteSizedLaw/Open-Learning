14c45ce3d162e4cfcef317819ea5c3cac593f475
CyberFinn <stephendiavolo@gmail.com>
2024-08-10 17:36:01 +0200

    Fix: Adjustments for Github Pages limitations

--- js/contractors.js
+++ js/contractors.js
@@ -2,15 +2,24 @@ window.onload = function() {
     var fileDisplayArea = document.getElementById('pageText');
 
     // Read the file -> we do this to keep our HTML files small, we dont want massive walls of text. It also makes "making changes" easier and simpler for non-technical people
-    fetch('https://raw.githubusercontent.com/Cyber-Finn/Blog/main/text/Contractors/Contractors.txt') //This link has CORS, but the original doesnt https://github.com/Cyber-Finn/Blog/blob/main/text/Contractors/Contractors.txt lol
-        .then(response => response.text())
-        .then(fileContent => {
-            // Handle the file content here
-            //console.log(fileContent);
-            fileDisplayArea.text = fileContent;
-        })
-        .catch(error => {
-            console.error('Error fetching the file:', error);
-            fileDisplayArea.textContent = 'Error displaying the info';
-        });
+    // fetch('https://raw.githubusercontent.com/Cyber-Finn/Blog/main/text/Contractors/Contractors.txt') //This link has CORS, but the original doesnt https://github.com/Cyber-Finn/Blog/blob/main/text/Contractors/Contractors.txt lol
+    //     .then(response => response.text())
+    //     .then(fileContent => {
+    //         // Handle the file content here
+    //         //console.log(fileContent);
+    //         fileDisplayArea.text = fileContent;
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching the file:', error);
+    //         fileDisplayArea.textContent = 'Error displaying the info';
+    //     });
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/Cyber-Finn/Blog/main/text/Contractors/Contractors.txt');
+        xhr.setRequestHeader("Accept", "application/vnd.github.3.raw");
+        xhr.send();
+
+        xhr.onload = function(e){
+            fileDisplayArea.textContent = xhr.response
+        }
  };
