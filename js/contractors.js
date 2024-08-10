5283085ff8097f572a663bed6ca095111bb5d4a8
CyberFinn <stephendiavolo@gmail.com>
2024-08-10 17:21:22 +0200

    Fix: Adjustments for Github Pages

--- js/contractors.js
+++ js/contractors.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var fileDisplayArea = document.getElementById('pageText');
 
     // Read the file -> we do this to keep our HTML files small, we dont want massive walls of text. It also makes "making changes" easier and simpler for non-technical people
-    fetch('../Text/Contractors/Contractors.txt')
+    fetch('https://raw.githubusercontent.com/Cyber-Finn/Blog/main/text/Contractors/Contractors.txt') //This link has CORS, but the original doesnt https://github.com/Cyber-Finn/Blog/blob/main/text/Contractors/Contractors.txt lol
         .then(response => response.text())
         .then(fileContent => {
             // Handle the file content here
