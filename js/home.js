d30f8e001bea317c2c13279b844d34cfd6e0011a
CyberFinn <stephendiavolo@gmail.com>
2024-08-18 12:17:20 +0200

    Fix: Refactored entire architecture Description: We dont want to reload the entire page, only a part of it! I reworked the anchors (which originally pointed us to new pages, with new js, new logic, etc.) to use an onclick instead that updates the current page without reloading, for a truly responsive and dynamic site. Massive QoL update

---
+++ js/home.js
@@ -0,0 +1,42 @@
+function updateBodyText(input) {
+    var fileDisplayArea = document.getElementById('pageText');
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', input);
+        xhr.setRequestHeader("Accept", "application/vnd.github.3.raw");
+        xhr.send();
+
+        xhr.onload = function(e){
+            fileDisplayArea.innerHTML = parseMarkdown(xhr.response)
+        }
+ };
+
+ function parseMarkdown(markdownText) {
+    // Replace Markdown headings (e.g., # Heading) with HTML <h1> tags
+    markdownText = markdownText.replace(/^# (.+)/gm, '<h1>$1</h1>');
+
+    // Display text prefixed with "##" in large headers
+    markdownText = markdownText.replace(/##([^#\n]+)/g, '<h2>$1</h2>');
+
+    // Display text prefixed with "###" in smaller headers
+    markdownText = markdownText.replace(/###([^#\n]+)/g, '<h3>$1</h3>');
+
+    // Add line breaks for <br>
+    markdownText = markdownText.replace(/<br>/g, '<br />');
+
+    // Convert Markdown links to HTML anchor tags
+    markdownText = markdownText.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
+
+    // Handle unordered list items (asterisk at the start of the line)
+    markdownText = markdownText.replace(/^\*\s+(.+)/gm, '<ul><li>$1</li></ul>');
+
+    // Handle nested unordered list items (asterisk at the start with optional spaces)
+    markdownText = markdownText.replace(/^\s*\*\s+(.+)/gm, '<ul><li>$1</li></ul>');
+
+    // Handle asterisks for bold text
+    markdownText = markdownText.replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>');
+
+    // Handle underscores for italic text
+    //markdownText = markdownText.replace(/_([^_]+)_/g, '<em>$1</em>');
+
+    return markdownText;
+}
